b08b2fa30348446a3dd7a773ac4ef9be1c7be46b
Gavin Stark <gavin@gstark.com>
2015-09-28 12:06:20 -0400

    Adds a practice for classes

---
+++ classes/README.md
@@ -0,0 +1,6 @@
+To run the code:
+  `ruby test_classes.rb`
+
+Remove the `skip` from the first test, make that test pass by changing the code in `classes.rb` -- do not change the code in `test_classes.rb` except for removing `skip` lines.
+
+NOTE: You may have to go back and change code you wrote previously to make a new test pass. Don't be afraid to change the code in `classes.rb` as needed, but run the tests *often* to make sure you aren't making an old test fail while making a new test pass.
---
+++ classes/test_classes.rb
@@ -0,0 +1,123 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'minitest/spec'
+require_relative 'classes.rb'
+
+describe "Learning about Class" do
+  it "Define a Dog class" do
+    skip
+    assert_equal Class, Dog.class
+  end
+
+  it "Dog's should be initialized with their names" do
+    skip
+    dog = Dog.new("Riley")
+
+    assert_equal "Riley", dog.name
+  end
+
+  it "Dog's names can be changed after being created" do
+    skip
+    dog = Dog.new("Riley")
+
+    dog.name = "Fido"
+
+    assert_equal "Fido", dog.name
+  end
+
+  it "Dog's can bark" do
+    skip
+    dog = Dog.new("Riley")
+
+    assert_equal "woof!", dog.bark
+  end
+
+  it "Dog's know if they bark loud" do
+    skip
+    dog = Dog.new("Riley")
+
+    dog.bark_volume = :loud
+    assert_equal "WOOF!", dog.bark
+  end
+
+  it "Dog's know if they bark loud or soft" do
+    skip
+    dog = Dog.new("Riley")
+
+    assert_equal "woof!", dog.bark
+
+    dog.bark_volume = :loud
+    assert_equal "WOOF!", dog.bark
+
+    dog.bark_volume = :soft
+    assert_equal "woof!", dog.bark
+  end
+
+  it "There is a BigDog as well" do
+    skip
+    assert_equal Class, BigDog.class
+  end
+
+  it "BigDog is a subclass of Dog" do
+    skip
+    assert BigDog.ancestors.include?(Dog)
+  end
+
+  it "BigDogs also have names" do
+    skip
+    loud_big_dog = BigDog.new("Fido")
+
+    assert_equal "Fido", loud_big_dog.name
+  end
+
+  it "BigDog default bark_volume is :loud" do
+    skip
+    loud_big_dog = BigDog.new("Fido")
+
+    assert_equal "WOOF!", loud_big_dog.bark
+  end
+
+  it "Define a Lizard class that knows it's color" do
+    skip
+    lizard = Lizard.new("green")
+
+    assert_equal "green", lizard.color
+  end
+
+  it "When a big dog greets a red lizard it barks" do
+    skip
+    lizard = Lizard.new("red")
+    dog    = BigDog.new("Sparky")
+
+    # ignore this line
+    dog.stub(:bark, "You returned WOOF! but not by calling bark") do
+
+      # Here is the test
+      assert_equal dog.bark, dog.greet(lizard), "Oops, the dog should bark!"
+    end
+  end
+
+  it "When a big dog greets a green lizard it does nothing" do
+    skip
+    lizard = Lizard.new("green")
+    dog    = BigDog.new("Sparky")
+
+    assert_nil dog.greet(lizard)
+  end
+
+  it "When a little dog greets a red lizard it does nothing" do
+    skip
+    lizard = Lizard.new("red")
+    dog    = Dog.new("Riley")
+
+    assert_nil dog.greet(lizard)
+  end
+
+  it "When a little dog greets a green lizard it does nothing" do
+    skip
+    lizard = Lizard.new("green")
+    dog    = Dog.new("Riley")
+
+    assert_nil dog.greet(lizard)
+  end
+end
